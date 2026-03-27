7cad57aa8e5e43ea97dee973427a02288652a44e
Chad Moxness <moxtheox@yahoo.com>
2026-03-27 12:56:47 -0700

    Refactor HTTP400Error class to use generics for improved type safety

--- lib/classes/errors/HTTP/400.ts
+++ lib/classes/errors/HTTP/400.ts
@@ -1,7 +1,7 @@
 import { RejectedPromise } from "../rejectedPromise";
 
-export class HTTP400Error extends RejectedPromise<any> {
-    constructor(c:any) {
+export class HTTP400Error<T> extends RejectedPromise<T> {
+    constructor(c:T) {
         super(400,c); 
     } //TODO:Finish class
 }
